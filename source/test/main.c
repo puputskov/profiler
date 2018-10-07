4440b4752e054b19a5151d5e578db4476cf8b01f
Jyri Puputti <jyri.puputti@gmail.com>
2018-10-07 21:43:12 +0300

    ...

--- source/test/main.c
+++ source/test/main.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <Windows.h>
 #include <assert.h>
 #include <profiler/profiler.h>
+
+void sort (int32_t *v, int32_t n)
+{
+	PROFILER_BEGIN ()
+	int32_t i, j;
+	for (i = 0; i < n - 1; ++ i)
+	{
+		for (j = i + 1; j < n; ++ j)
+		{
+			if (i != j)
+			{
+				if (v [i] > v [j])
+				{
+					int32_t tmp = v [i];
+					v [i] = v [j];
+					v [j] = tmp;
+				}
+			}
+		}
+	}
+	PROFILER_END ()
+}
 
 void say_hello (const char *name)
 {
 	PROFILER_BEGIN()
 	printf ("Hello %s!\n", name);
-	Sleep (rand () % 100);
+	int32_t n = 1000;
+	int32_t v [1000];
+	int32_t i;
+	for (i = 0; i < n; ++ i)
+	{
+		v [i] = rand () % 10000;
+	}
+
+	sort (v, n);
+	int32_t last = v [0];
+	for (i = 0; i < n; ++ i)
+	{
+		assert (last <= v [i]);
+		last = v [i];
+	}
 	PROFILER_END()
 }
 
 int main (int argc, const char **argv)
 {
+	srand (time (NULL));
 	assert (profiler_init (PROFILER_TYPE_PROFILER, PROFILER_MAKE_ADDRESS (127, 0, 0, 1), 1672));
 
 	PROFILER_BEGIN()
